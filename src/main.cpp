48fbac8e14bdfc284337192752e6c24fe404343a
Beo <beo-coder@protonmail.com>
2024-03-03 15:01:38 +0100

    added letters.h

--- src/main.cpp
+++ src/main.cpp
@@ -2,47 +2,72 @@
 #include "hardware/pio.h"
 #include "hardware/dma.h"
 #include "hardware/irq.h"
-#include "pioARGB.pio.h"
 
+#include "letters.h"
 #include "pioMatrixOutput.h"
+
 
 PIO pio = pio0;
 
+
 MatrixOutput ledMatrix(pio, 0, 0, 10, 11);
+
+uint32_t display[8][16];
 
 
 void setup() {
-
     color color1{};
+    color1.red = 50;
+    color1.green = 0;
+    color1.blue = 0;
 
+    color color2{};
+    color2.red = 0;
+    color2.green = 0;
+    color2.blue = 0;
+
+    for(int i=0;i<5;i++){
+        for(int j=0; j<8;j++){
+            if((lA[j] >> i) & 0x01){
+                display[j][i] = *(uint32_t *) &color1;
+            }else{
+                display[j][i] = *(uint32_t *) &color2;
+            }
+
+        }
+    }
 
     for (int i = 0; i < 128; i++) {
 
+            ledMatrix.matrix[i/64][i % 64] = display[i/16][i%16];
+
+    }
+    ledMatrix.sendData();
+    /*
+
+
+    for (int i = 0; i < 128; i++) {
         color1.red = 255;
         color1.green = 0;
         color1.blue = 0;
 
 
         ledMatrix.matrix[i / 64][i % 64] = *(uint32_t *) &color1;
-
     }
     ledMatrix.sendData();
 
 
     delay(1000);
     for (int i = 0; i < 128; i++) {
-
         color1.red = 100;
-        color1.green = 100;
-        color1.blue = 0;
+        color1.green = 0;
+        color1.blue = 100;
 
 
         ledMatrix.matrix[i / 64][i % 64] = *(uint32_t *) &color1;
-
     }
     ledMatrix.sendData();
-
-
+     */
 
 
     //Serial1.begin(115200);
@@ -92,13 +117,9 @@ void setup() {
 
     delay(1000);
     pio_sm_set_enabled(pio, 0, false);
-
-
 }
 
 
 void loop() {
     tight_loop_contents();
-
-
 }
